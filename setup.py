fbfc5735e05b2051a9bca973c3131d452f0ac5cf
Prashant Agarwal <agarwalprashant355@gmail.com>
2025-10-01 10:54:58 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a small ml project for emotion detection with aws s3 as remote dvc storage',
+    author='prashant',
+    license='',
+)
